d88093b8cc3b4db50c055c15b90bbee5329f541b
adityas1netapp <Aditya.Singh@flexera.com>
2025-06-12 12:48:46 +0530

    Running Load Test on Specific Load

--- load-test.js
+++ load-test.js
@@ -4,18 +4,22 @@ import { sleep } from 'k6';
 export const options = {
     stages: [
         {
-            duration: '10s',
-            target: 10
-        },
-        {
-            duration: '60s',
+            duration: '5s',
             target: 10
         },
         {
             duration: '10s',
+            target: 10
+        },
+        {
+            duration: '5s',
             target: 0
         },
-    ]
+    ],
+
+    cloud: {
+        projectID : __PROJECT_ID__
+    }
 };
 
 export default function () {
