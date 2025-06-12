a7bb5dba3ddf9afe6861f7b77b81d280ddd78053
adityas1netapp <Aditya.Singh@flexera.com>
2025-06-12 11:29:56 +0530

    Add first k6 performance test script

---
+++ first-script.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 10,
+    duration: '30s',
+    cloud: {
+        projectID : 3776352
+    }
+}
+
+export default function () {
+    http.get('https://test.k6.io');
+    sleep(1);
+}
